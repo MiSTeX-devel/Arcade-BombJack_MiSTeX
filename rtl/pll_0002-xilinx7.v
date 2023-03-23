87f42bbbacb9a3832e09c62762e34954ab5f10ee
Hans Baier <hansfbaier@gmail.com>
2023-03-23 17:16:11 +0700

    properly implement core PLL

--- rtl/pll_0002-xilinx7.v
+++ rtl/pll_0002-xilinx7.v
@@ -8,31 +8,32 @@ module  pll_0002(
 	output wire locked
 );
 
-	wire feedback;
+	wire feedback1, feedback2;
+	wire cascade;
 	
-	PLLE2_ADV #(
-		.CLKFBOUT_MULT(6'd24),
+	MMCME2_ADV #(
+		.CLKFBOUT_MULT_F(6'd12),
 		.CLKIN1_PERIOD(20.0),
-		.CLKOUT0_DIVIDE(7'd25), // 48 MHz = 50 MHz * 24 / 25
+		.CLKOUT0_DIVIDE_F(12.5), // 48 MHz = 50 MHz * 12 / 12.5
 		.CLKOUT0_PHASE(1'd0),
-		.CLKOUT1_DIVIDE(8'd200), // 6 MHz = 50 MHz * 24 / 200
+		.CLKOUT1_DIVIDE(100), // 6 MHz = 50 MHz * 12 / 100
 		.CLKOUT1_PHASE(1'd0),
-		.CLKOUT2_DIVIDE(8'd50), // 24 MHz = 50 MHz * 24 / 50
+		.CLKOUT2_DIVIDE(25), // 24 MHz = 50 MHz * 12 / 25
 		.CLKOUT2_PHASE(1'd0),
 		.DIVCLK_DIVIDE(1'd1),
 		.REF_JITTER1(0.01),
 		.STARTUP_WAIT("FALSE")
-	) PLLE2_ADV (
-		.CLKFBIN(feedback),
+	) BOMBJACK_PLL (
+		.CLKFBIN(feedback1),
 		.CLKIN1(refclk),
 		.PWRDWN(1'b0),
 		.RST(rst),
-		.CLKFBOUT(feedback),
+		.CLKFBOUT(feedback1),
 		.CLKOUT0(outclk_0),
 		.CLKOUT1(outclk_1),
 		.CLKOUT2(outclk_2),
 		.LOCKED(locked)
-);
+	);
 	
 endmodule
 
