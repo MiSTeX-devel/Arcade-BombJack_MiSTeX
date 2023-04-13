49b48cef7c5c6025f0fa1720232acb06ca9ba336
Hans Baier <hansfbaier@gmail.com>
2023-04-13 09:05:04 +0700

    use template sys, add hdmi_config and add altpll option

--- rtl/pll_0002.v
+++ rtl/pll_0002.v
@@ -8,6 +8,7 @@ module  pll_0002 (
 	output wire locked
 );
 
+`ifdef CYCLONEV
 	altera_pll #(
 		.fractional_vco_multiplier("false"),
 		.reference_clock_frequency("50.0 MHz"),
@@ -32,5 +33,37 @@ module  pll_0002 (
 		.fbclk	(1'b0),
 		.refclk	(refclk)
 	);
+	);
+`else
+	ALTPLL #(
+		.BANDWIDTH_TYPE("AUTO"),
+		.CLK0_DIVIDE_BY(10'd27),   // 48 MHz ≈ 50 MHz * 26 / 27
+		.CLK0_DUTY_CYCLE(6'd50),
+		.CLK0_MULTIPLY_BY(10'd26),
+		.CLK0_PHASE_SHIFT(1'd0),
+		.CLK1_DIVIDE_BY(10'd54),   // 24 MHz ≈ 50 MHz * 26 / 54
+		.CLK1_DUTY_CYCLE(6'd50),
+		.CLK1_MULTIPLY_BY(10'd26),
+		.CLK1_PHASE_SHIFT(1'd0),
+		.CLK2_DIVIDE_BY(10'd216),   // 6 MHz ≈ 50 MHz * 26 / 216
+		.CLK2_DUTY_CYCLE(6'd50),
+		.CLK2_MULTIPLY_BY(10'd26),
+		.CLK2_PHASE_SHIFT(1'd0),
+		.COMPENSATE_CLOCK("CLK0"),
+		.INCLK0_INPUT_FREQUENCY(15'd20000),
+		.OPERATION_MODE("NORMAL")
+	) ALTPLL (
+		.ARESET(1'd0),
+		.CLKENA(5'd31),
+		.EXTCLKENA(4'd15),
+		.FBIN(1'd1),
+		.INCLK(refclk),
+		.PFDENA(1'd1),
+		.PLLENA(1'd1),
+		.CLK({outclk_2, outclk_1, outclk_0}),
+		.LOCKED(locked)
+	);
+`endif
+
 endmodule
 
